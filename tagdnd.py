121f4ece9a0df65e89880d8e2a85f1ecc6a79778
giodod <giovanni@doderos.net>
2015-10-18 08:52:04 +0200

    [Double click open tag in filebrowser]

--- tagdnd.py
+++ tagdnd.py
@@ -1,17 +1,17 @@
-// Copyright 2015 Giovanni Dodero.
+# Copyright 2015 Giovanni Dodero.
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wx
 import subprocess as sub
@@ -34,6 +34,17 @@ class MyFileDropTarget(wx.FileDropTarget):
         font = wx.Font(18, wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
         myTextAttr.SetFont(font)
 	self.fileTextCtrl.SetStyle(0,self.fileTextCtrl.GetLastPosition(),myTextAttr)
+	self.fileTextCtrl.Bind(wx.EVT_LEFT_DCLICK,self.MyDblClick);
+    #----------------------------------------------------------------------
+    def MyDblClick(self, text):
+        """
+        To reuse box you must fill again begin data
+        """
+	print(self.mylabel+" cliccato!")
+	#open mount point in vfs
+	output=sub.check_output("xdg-open ~/taggati/tags/"+self.mylabel, shell=True)
+	print(output)
+
     #----------------------------------------------------------------------
     def CleanStart(self, mylabel):
         """
